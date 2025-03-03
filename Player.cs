a93bac6de88e7960a4315a6bd90d4367aa4c3ae7
TafaraMangombe <tafaramangombe06@gmail.com>
2025-03-03 22:40:29 +0000

    added alot

--- Player.cs
+++ Player.cs
@@ -1,25 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DungeonExplorer
 {
     public class Player
+
     {
-        public string Name { get; private set; }
-        public int Health { get; private set; }
+        public string _Name;
+        public int _Health = 100;
         private List<string> inventory = new List<string>();
 
+        public string Name 
+        { 
+            get
+            {
+                return _Name;
+            }
+
+            private set
+            {
+                _Name = value;
+            }
+        }
+        public int Health
+        {
+            get
+            {
+                return _Health;
+            }
+
+            private set
+            {
+                if (_Health == 100)
+                    Console.WriteLine($"{_Name} is at full health");
+                else
+                    _Health = value;
+            }
+        }
+            
+          
+    
         public Player(string name, int health) 
         {
-            Name = name;
-            Health = health;
+            this.Name = name;
+            this.Health = health;
         }
         public void PickUpItem(string item)
         {
-
+          
         }
         public string InventoryContents()
         {
             return string.Join(", ", inventory);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Name");
+            String usersName = Console.ReadLine();
+            Player player = new Player(usersName,100);
+
+
         }
     }
 }
